0b0552a0c4393c1e273ea1314b45ea7a9cbe4e20
MOD <mask_of_doom@mail.ru>
2024-11-12 23:11:36 +0300

    пробую вносить изменения

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-
+age = int(input())
+if age <= 13:
+    print("детство")
+if 14 <= age <= 24:
+    print("молодость")
+if 25 <= age <= 59:
+    print("зрелость")
+if 60 <= age <= 120:
+    print("старость")
+if age > 120:
+    print("смэрть")
